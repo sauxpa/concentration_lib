48de8844185e5be4c3a5b05e4c1475f7885d809d
sauxpa <patrick.jr.saux@gmail.com>
2021-11-10 15:56:47 +0100

    setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(
     name='concentration_lib',
     packages=['concentration_lib'],
-    version='0.1.0',
+    version='0.1.1',
     author="Patrick Saux",
     author_email="patrick.saux@ginria.fr",
     description="Library for concentration bounds",
